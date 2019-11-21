b248b53fcb9e2cce962330204219070e80c8943b
adhikariastha5 <adhikari.astha5@gmail.com>
2019-11-22 00:30:24 +0545

    Add some word lists

---
+++ Words/list.py
@@ -0,0 +1,8 @@
+A=['abate','aberrant','abeyance','abscond','abstemious','admonish','adulterate','aesthetic','aggregate','alacrity','alleviate','amalgamate','ambiguous','ambivalaence','ameliorate','anachronism','analogous','anarchy','anomalous','antipathy','apathy','appease','apprise','approbation','appropriate','arduous','artless','ascetic','assiduous','assuage','attenuate','audacious','austere','autonomous','aver']
+
+B=['banal','belie','beneficient','bolster','bombastic','boorish','burgeon','burnish','buttress']
+
+C=['capricious','castigation','catalyst','caustic','chicanery','coagulate','coda','cogent','commensurate','compendium','complaisant','complaint','concilatory','condone','confound','connoisseur','contention','contentious','contrite','conundrum','converge','convoluted','craven']
+
+
+
---
+++ Words/randomtext.py
@@ -0,0 +1,5 @@
+import list
+import random
+
+all=list.A+list.B+list.C
+print(random.choice(all))
